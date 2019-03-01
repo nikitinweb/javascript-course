bf012bf1b69f2a96fc09c21766f8f3730801c5f8
Alexander Nikitin <anikitin.web@gmail.com>
2019-03-01 12:24:49 +0300

    create ls2 task 1

--- lesson2/task1/script.js
+++ lesson2/task1/script.js
@@ -1,30 +1,75 @@
 window.addEventListener('DOMContentLoaded', () => {
-	'use strict';
+  'use strict';
 
-	let money = +prompt("Ваш бюджет на месяц?", 10000),
-		time = prompt("Введите дату в формате YYYY-MM-DD", "1990-01-01"),
-		numberDay = 30,
-		appData = {
-			budjet: money,
-			timeData: time,
-			expenses: {},
-			optionalExpenses: {},
-			income: 0,
-			savings: false
-		};
+  let money = +prompt("Ваш бюджет на месяц?", 10000),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "1990-01-01"),
+    numberDay = 30,
+    appData = {
+      budget: money,
+      timeData: time,
+      expenses: {},
+      optionalExpenses: {},
+      income: 0,
+      savings: false
+    };
 
-	for (let i = 0; i < 2; i++) {
-		let expensesName = prompt("Введите обязательную статью расходов в этом месяце", "Налоги"),
-			expensesTotal = +prompt("Во сколько обойдется", 3000);
+  for (let i = 0; i < 2; i++) {
+    let expensesName = prompt("Введите обязательную статью расходов в этом месяце", "Налоги"),
+      expensesTotal = +prompt("Во сколько обойдется", 3000);
 
-		if ((typeof (expensesName)) === 'string' && (typeof (expensesName)) != null && (typeof (expensesTotal)) === 'number' && (typeof (expensesTotal)) != null && expensesName != '' && expensesTotal != '') {
-			appData.expenses[expensesName] = expensesTotal;
-		} else {
+    if ((typeof (expensesName)) === 'string' && (typeof (expensesName)) != null && (typeof (expensesTotal)) === 'number' && (typeof (expensesTotal)) != null && expensesName != '' && expensesTotal != '') {
+      appData.expenses[expensesName] = expensesTotal;
+    } else {
+      i--;
+      alert('Поля заполнены не правильно. Попробуйте еще раз');
+      continue;
+    }
+  }
 
-		}
-	}
+  // Цикл while
+  // let expensesNum = 0;
 
-	appData.moneyPerDay = Math.floor(appData.budjet / numberDay);
+  // while (expensesNum < 2) {
+  //   let expensesName = prompt("Введите обязательную статью расходов в этом месяце", "Налоги"),
+  //     expensesTotal = +prompt("Во сколько обойдется", 3000);
 
-	alert("Бюджет на день: " + appData.moneyPerDay + " руб");
+  //   if ((typeof (expensesName)) === 'string' && (typeof (expensesName)) != null && (typeof (expensesTotal)) === 'number' && (typeof (expensesTotal)) != null && expensesName != '' && expensesTotal != '') {
+  //     appData.expenses[expensesName] = expensesTotal;
+  //     expensesNum++;
+  //   } else {
+  //     alert('Поля заполнены не правильно. Попробуйте еще раз');
+  //     continue;
+  //   }
+  // }
+
+
+  // Цикл do while
+  // let expensesNum = 0;
+
+  // do {
+  //   let expensesName = prompt("Введите обязательную статью расходов в этом месяце", "Налоги"),
+  //     expensesTotal = +prompt("Во сколько обойдется", 3000);
+
+  //   if ((typeof (expensesName)) === 'string' && (typeof (expensesName)) != null && (typeof (expensesTotal)) === 'number' && (typeof (expensesTotal)) != null && expensesName != '' && expensesTotal != '') {
+  //     appData.expenses[expensesName] = expensesTotal;
+  //     expensesNum++;
+  //   } else {
+  //     alert('Поля заполнены не правильно. Попробуйте еще раз');
+  //     continue;
+  //   }
+  // } while (expensesNum < 2);
+
+  appData.moneyPerDay = Math.floor(appData.budget / numberDay);
+
+  alert("Бюджет на день: " + appData.moneyPerDay + " руб");
+
+  if (appData.moneyPerDay < 100) {
+    console.log('Минимальный уровень достатка');
+  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Средний уровень достатка');
+  } else if (appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень достатка');
+  } else {
+    console.log('Произошла ошибка');
+  }
 });
