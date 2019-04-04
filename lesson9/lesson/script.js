74c36a6ba9ee39e89be82f767023a58eba42eafc
Alexander Nikitin <anikitin.web@gmail.com>
2019-04-04 20:15:15 +0300

    create ls9

---
+++ lesson9/lesson/script.js
@@ -0,0 +1,102 @@
+// function User(name, id) {
+// 	this.name = name;
+// 	this.id = id;
+// 	this.human = true;
+// 	this.hello = function () {
+// 		console.log('Hello! ' + this.name);
+// 	}
+// }
+
+// User.prototype.exit = function (name) {
+// 	console.log('Пользователь ' + this.name + ' ушел');
+// }
+
+// let ivan = new User('Ivan', 25),
+// 	alex = new User('Alex', 20);
+
+// // console.log(ivan);
+// // console.log(alex);
+
+// ivan.exit();
+
+// class User {
+// 	constructor(name, id) {
+// 		this.name = name;
+// 		this.id = id;
+// 		this.human = true;
+// 	}
+// 	hello() {
+// 		console.log(`Hello! ${this.name}`);
+// 	}
+// 	exit() {
+// 		console.log(`Пользователь ${this.name} ушел`);
+// 	}
+// }
+
+// let ivan = new User('Ivan', 25);
+// let alex = new User('Alex', 20);
+
+// ivan.exit();
+
+// 'use strict';
+
+// function showThis(a, b) {
+// 	console.log(this);
+// 	function sum() {
+// 		console.log(this);
+// 		return a + b;
+// 	}
+// 	console.log(sum());
+// }
+
+// showThis(4, 5);
+
+// let obj = {
+// 	a: 20,
+// 	b: 15,
+// 	sum: function () {
+// 		console.log(this);
+// 		function shout() {
+// 			console.log(this);
+// 		}
+// 		shout();
+// 	}
+// };
+
+// obj.sum();
+
+// let user = {
+// 	name: "John"
+// }
+
+// function sayName(surname) {
+// 	console.log(this);
+// 	console.log(this.name + surname);
+// }
+
+// console.log(sayName.call(user, 'Smith'));
+// console.log(sayName.apply(user, ['Snow']));
+
+// function count(number) {
+// 	return this * number;
+// }
+
+// let double = count.bind(2);
+// console.log(double(3));
+// console.log(double(10));
+
+let btn = document.querySelector('button');
+btn.addEventListener('click', function () {
+  console.log(this);
+  this.style.backgroundColor = 'red';
+
+  function showThis() {
+    console.log(this);
+  }
+  showThis();
+});
+
+// 1) Просто вызов функции - window/undefined
+// 2) Метод обьекта - this = обьект
+// 3) Конструктов (new) - this = новый созданный обьект
+// 4) Указание конкретного контекста - call, apply, bind
---
+++ lesson9/task2/script.js
@@ -0,0 +1,11 @@
+let age = document.getElementById('age');
+let user = {
+  name: 'Иван',
+  surname: 'Иванов'
+}
+
+function showUser(surname, name) {
+  alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+}
+
+showUser.call(age, [user.name], [user.surname]);
